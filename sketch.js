5e409c362993882f35e3ca9f08c6e11062a39501
keerthikiran2585 <79265713+keerthikiran2585@users.noreply.github.com>
2021-05-26 14:07:22 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,56 @@
+var ball;
+var hball, database, position;
+
+function setup(){
+    database=firebase.database();
+    createCanvas(500,500);
+    hball = createSprite(250,250,10,10);
+    hball.shapeColor = "red";
+
+    var hballPosition=database.ref('ball/position');
+    hballPosition.on("value", readPosition);
+
+
+}
+
+function draw(){
+    background("white");
+
+    if(position!==undefined){
+        
+
+    
+    if(keyDown(LEFT_ARROW)){
+        writePosition(-1,0);
+    }
+    else if(keyDown(RIGHT_ARROW)){
+        writePosition(1,0);
+    }
+    else if(keyDown(UP_ARROW)){
+        writePosition(0,-1);
+    }
+    else if(keyDown(DOWN_ARROW)){
+        writePosition(0,+1);
+    }
+    drawSprites();
+}
+}
+
+function changePosition(x,y){
+    ball.x = ball.x + x;
+    ball.y = ball.y + y;
+}
+
+function readPosition(data){
+    position=data.val();
+    hball.x=position.x;
+    hball.y=position.y;
+
+}
+
+function writePosition(x, y){
+    database.ref('ball/position').set({
+        'x':position.x+x, 
+        'y':position.y+y
+    })
+}
